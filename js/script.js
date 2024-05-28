c9c03a1f6482d7fc3f57cc66086095a707279651
Andreaesther <eaa23aesmp@students.eaaa.dk>
2024-05-28 09:51:27 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 function scrollLeft() {
     const slider = document.querySelector('.slider');
+    const sliderItem = document.querySelector('.slider-item');
+    const scrollAmount = sliderItem.offsetWidth; 
     slider.scrollBy({
-        left: -540,
+        left: -scrollAmount,
         behavior: 'smooth'
     });
 }
 
 function scrollRight() {
     const slider = document.querySelector('.slider');
+    const sliderItem = document.querySelector('.slider-item');
+    const scrollAmount = sliderItem.offsetWidth; 
     slider.scrollBy({
-        left: 540,
+        left: scrollAmount,
         behavior: 'smooth'
     });
 }
